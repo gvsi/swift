738cc0db3d93fa2a15e87f93aca5d50108a3c5f7
Giovanni Alcantara <me@giovannialcantara.com>
2014-06-03 13:22:44 +0200

    Add prime numbers section

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -2,4 +2,31 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+
+// Calculates the first 100 prime numbers
+var primeNumbers = Int[]()
+
+func isPrime(number: Int) -> Bool {
+    var isPrime = true
+    for i in 2..Int(floor(sqrt(Double(number))))+1 {
+        if number % i == 0 {
+            isPrime = false
+            break
+        }
+    }
+    if isPrime == true {
+        return true
+    } else {
+        return false
+    }
+}
+
+var i = 1;
+while primeNumbers.count < 100 {
+    if isPrime(i) {
+        primeNumbers.append(i);
+    }
+    ++i
+}
+
+primeNumbers
